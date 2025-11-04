914d016e3a0a6eef5d4a7ac8978021e33deaee8e
LauraSpy <137756546+LauraSpy@users.noreply.github.com>
2025-11-04 20:27:06 +0100

    exercice scanner ok

--- python_exo/scan.py
+++ python_exo/scan.py
@@ -11,3 +11,38 @@ def scan(ip_target, port_target):
             print("OK")
         except (TimeoutError, ConnectionRefusedError) as e:
             print(e)
+
+
+def validate_port(port):
+    try:
+        p = int(port)
+        if p in range(0, 65536):
+            return True
+        else:
+            return False
+    except ValueError as e:
+        return False
+
+
+def validate_ip(ip):
+    adress = ip.split(".")
+    if len(adress) == 4:
+        for element in adress:
+            try:
+                e = int(element)
+                if not (0 <= e <= 256):
+                    return False
+            except ValueError as e:
+                return False
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    # Test validate_port
+    print("Port 65535:", validate_port("65535"))  # Doit être True
+
+    # Test validate_ip
+    print("IP 192.168.1.1:", validate_ip("192.168.1.1"))  # Doit être True
+    print("IP 256.1.1.1:", validate_ip("256.1.1.1"))  # Doit être False
