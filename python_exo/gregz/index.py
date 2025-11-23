0135d44ab652b33c970767ead0a2c4ae206c1b84
LauraSpy <137756546+LauraSpy@users.noreply.github.com>
2025-11-23 19:33:32 +0100

    début

--- python_exo/gregz/index.py
+++ python_exo/gregz/index.py
@@ -46,10 +46,15 @@
 # rajouter du css pour faire joli
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("hello.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
